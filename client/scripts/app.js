d2a53ae643097b9ac6071e684f74c98b8cba9606
George Humphrey <george.p.humphrey@gmail.com>
2020-02-06 06:15:16 -0500

    Connected to database

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,22 +1,70 @@
-var App = {
-  restaurantID: 1,
+const React = require('react');
 
-  initialize: function() {
-    // const url = window.location.href;
-    // const extension = //end of url
-    // App.restaurantID = extension
+class ReviewSection extends React.Component {
+  constructor(props) {
+    super(props);
 
-    allReviews.initialize();
-
-    // Fetch reviews
-    App.fetch();
-  },
-
-  fetch: function(callback = () => {}) {
-    Parse.readAll(data => {
-      // examine the response from the server request:
-
-      callback(JSON.parse(data));
-    });
+    this.state = {};
   }
-};
+
+  componentDidMount() {}
+
+  render() {
+    return (
+      <div className='allReviews'>
+        <div className='reviewer'>
+          <img src='img/profile.png' className='profPic' />
+
+          <div className='profileText'>
+            <div className='profileInfo'>
+              <div className='name'>Zulema Z.</div>
+              <br />
+              <div className='location'>Yoder, WY</div>
+            </div>
+
+            <div className='friends counterBox'>
+              <img className='friendIcon icon' src='img/friendIcon.png' />
+              <div className='friendCount count'>123</div>
+              <div className='friendWord word'>friends</div>
+            </div>
+            <br />
+
+            <div className='reviews counterBox'>
+              <img className='reviewIcon icon' src='img/reviewIcon.png' />
+              <div className='reviewCount count'>456</div>
+              <div className='reviewWord word'>reviews</div>
+            </div>
+            <br />
+
+            <div className='photos counterBox'>
+              <img className='photoIcon icon' src='img/photoIcon.png' />
+              <div className='photoCount count'>789</div>
+              <div className='photoWord word'>photos</div>
+            </div>
+          </div>
+        </div>
+
+        <div className='review'>
+          <div className='stars'>
+            <img className='star' src='img/starIcon.png' />
+            <img className='star' src='img/starIcon.png' />
+            <img className='star' src='img/starIcon.png' />
+            <img className='star' src='img/starIcon.png' />
+            <img className='star' src='img/noStarIcon.png' />
+          </div>
+
+          <div className='date'>31/12/2020</div>
+          <br />
+
+          <div className='reviewText'>
+            Loved It! Like, Super-duper good. All the foods just
+            nomnomnomnomnomnomnomnom. Loved how they had vegan options, which is
+            not super common in restaurants in Kato.
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ReviewSection;
--- server/app.js
+++ server/app.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
-const port = 3000;
-
-// Set what we are listening on.
-app.set('port', 3000);
+const db = require('./db/index.js');
+const port = 3001;
 
 app.use('/', express.static('client'));
 
-app.get('/', (req, res) => res.send(''));
+db.connect(function(err) {
+  if (err) throw err;
+  db.query('SELECT * FROM reviewText', function(err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
+
+app.get('/', (req, res) => res.send());
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
